6e0206191bc93ac4e335e4d392e3b6b875b12ec9
Nikolai A. Kuropyatnikov <nickolyan@google.com>
2025-03-07 22:58:28 +0300

    docker postgres run

--- postgresql/init/000.sql
+++ postgresql/init/000.sql
@@ -1,20 +1,19 @@
-CREATE TABLE user (
-    id       int,
+CREATE TABLE "botusers" (
+    id       serial PRIMARY KEY,
     tid      bigint, -- telegram ID
-    uname    varchar(64), -- @username
-    lname    varchar(64), -- last name
-    fname    varchar(64), -- first name
+    uname    varchar(64) NOT NULL, -- @username
+    lname    varchar(64) NOT NULL, -- last name
+    fname    varchar(64) NOT NULL, -- first name
     rdate    timestamp,   -- registration date
     sender   boolean,     -- can send message to user
     reciver  boolean,     -- can recive message from user
     uadmin   boolean      -- user is admin
 );
-CREATE UNIQUE INDEX id ON user (id);
-CREATE UNIQUE INDEX tid ON user (tid);
-CREATE INDEX uname ON user (uname);
-CREATE INDEX lname ON user (lname);
-CREATE INDEX fname ON user (fname);
-CREATE INDEX rdate ON user (rdate);
-CREATE INDEX sender ON user (sender);
-CREATE INDEX reciver ON user (reciver);
-CREATE INDEX uadmin ON user (uadmin);
+CREATE UNIQUE INDEX tid ON "botusers" (tid);
+CREATE INDEX uname ON "botusers" (uname);
+CREATE INDEX lname ON "botusers" (lname);
+CREATE INDEX fname ON "botusers" (fname);
+CREATE INDEX rdate ON "botusers" (rdate);
+CREATE INDEX sender ON "botusers" (sender);
+CREATE INDEX reciver ON "botusers" (reciver);
+CREATE INDEX uadmin ON "botusers" (uadmin);
